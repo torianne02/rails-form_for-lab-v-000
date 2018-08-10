fbeb7eb445ba240ed9dc2052364b6612d6ae6212
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-08-10 21:13:04 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,7 +9,7 @@ class StudentsController < ApplicationController
 
   def create 
     @student = Student.new(params.require(:student).permit(:first_name, :last_name))
-    @student.save 
+    @student.save
     redirect_to student_path(@student)
   end 
 
@@ -19,7 +19,7 @@ class StudentsController < ApplicationController
 
   def update 
     @student = Student.find(params[:id])
-    @student.update(params.require(:student))
+    @student.update(params.require(:student).permit(:first_name, :last_name))
     redirect_to student_path(@student)
   end
 end
