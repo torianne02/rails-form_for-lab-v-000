ca7438070064bdb15ae2f7813695b375300dd93e
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-08-10 21:12:30 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,9 +8,7 @@ class StudentsController < ApplicationController
   end 
 
   def create 
-    @student = Student.new 
-    @student.first_name = params[:first_name]
-    @student.last_name = params[:last_name]
+    @student = Student.new(params.require(:student).permit(:first_name, :last_name))
     @student.save 
 
     redirect_to student_path(@student)
