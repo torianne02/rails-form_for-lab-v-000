553a4690e09dbb282ca5c444bcfcec4867caf7f2
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-08-10 21:11:07 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -8,7 +8,7 @@ class SchoolClassesController < ApplicationController
   end 
 
   def create 
-    @school_class = SchoolClass.new
+    @school_class = SchoolClass.new(params.require(:school_class).permit(:title, :room_number))
     @school_class.title = params[:title]
     @school_class.room_number = params[:room_number]
     @school_class.save 
