26f84a07b06983c41e90c74dc71c8e5e7d834372
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-08-10 20:44:17 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,4 +1,8 @@
 class SchoolClassesController < ApplicationController
+  def show 
+    @school_class = SchoolClass.find(params[:id])
+  end 
+
   def new 
     @school_class = SchoolClass.new
   end 
@@ -10,10 +14,6 @@ class SchoolClassesController < ApplicationController
     @school_class.save 
 
     redirect_to school_class_path(@school_class)
-  end 
-
-  def show 
-    @school_class = SchoolClass.find(params[:id])
   end 
 
   def edit
